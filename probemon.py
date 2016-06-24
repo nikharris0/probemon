3ec19effce84d60b8e5ba02ae8d43ab518cb8bbb
Nik Harris <nik.harris0@gmail.com>
2016-06-23 20:44:30 -0700

    rssi support added, although not robust. will improve later

--- probemon.py
+++ probemon.py
@@ -7,18 +7,23 @@ import netaddr
 import sys
 import logging
 from scapy.all import *
+from pprint import pprint
 from logging.handlers import RotatingFileHandler
 
 
 NAME = 'probemon'
 DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
+DEBUG = False
 
-def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid):
+def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
 	def packet_callback(packet):
+		
 		if not packet.haslayer(Dot11):
 			return
 
+		# we are looking for management frames with a probe subtype
+		# if neither match we are done here
 		if packet.type != 0 or packet.subtype != 0x04:
 			return
 
@@ -46,6 +51,10 @@ def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid):
 		# include the SSID in the probe frame
 		if ssid:
 			fields.append(packet.info)
+			
+		if rssi:
+			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
+			fields.append(str(rssi_val))
 
 		logger.info(delimiter.join(fields))
 
@@ -61,11 +70,15 @@ def main():
 	parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
 	parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
 	parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
+	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
+	parser.add_argument('-D', '--debug', action='store_true', help="enable debug output")
 	args = parser.parse_args()
 
 	if not args.interface:
 		print "error: capture interface not given, try --help"
 		sys.exit(-1)
+	
+	DEBUG = args.debug
 
 	# setup our rotating logger
 	logger = logging.getLogger(NAME)
@@ -73,7 +86,8 @@ def main():
 	handler = RotatingFileHandler(args.output, maxBytes=args.max_bytes, backupCount=args.max_backups)
 	logger.addHandler(handler)
 
-	built_packet_cb = build_packet_callback(args.time, logger, args.delimiter, args.mac_info, args.ssid)
+	built_packet_cb = build_packet_callback(args.time, logger, 
+		args.delimiter, args.mac_info, args.ssid, args.rssi)
 	sniff(iface=args.interface, prn=built_packet_cb, store=0)
 
 
