34411135587b026ee69e834dceb1878295682504
Denis MacPherson <denismacpherson@gmail.com>
2017-03-15 18:22:01 -0800

    Update probemon.py

--- probemon.py
+++ probemon.py
@@ -9,54 +9,58 @@ import logging
 from scapy.all import *
 from pprint import pprint
 from logging.handlers import RotatingFileHandler
-
+import numpy
 
 NAME = 'probemon'
 DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
 DEBUG = False
 
+
 def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
 	def packet_callback(packet):
 		
-		if not packet.haslayer(Dot11):
-			return
+if not packet.haslayer(Dot11):
+                        return
 
-		# we are looking for management frames with a probe subtype
-		# if neither match we are done here
-		if packet.type != 0 or packet.subtype != 0x04:
-			return
+                # we are looking for management frames with a probe subtype
+                # if neither match we are done here
+                if packet.type != 0 or packet.subtype != 0x04:
+                        return
+                if packet.addr2 in exclude:
+                        return
 
-		# list of output fields
-		fields = []
+                # list of output fields
+                fields = []
 
-		# determine preferred time format 
-		log_time = str(int(time.time()))
-		if time_fmt == 'iso':
-			log_time = datetime.datetime.now().isoformat()
+                # determine preferred time format
+                log_time = str(int(time.time()))
+                if time_fmt == 'iso':
+                        log_time = datetime.now().isoformat()
 
-		fields.append(log_time)
+                #fields.append('{"timestamp":')
+                fields.append('"' + log_time + '"')
 
-		# append the mac address itself
-		fields.append(packet.addr2)
+                # append the mac address itself
+                fields.append('"' + packet.addr2 + '"')
 
-		# parse mac address and look up the organization from the vendor octets
-		if mac_info:
-			try:
-				parsed_mac = netaddr.EUI(packet.addr2)
-				fields.append(parsed_mac.oui.registration().org)
-			except netaddr.core.NotRegisteredError, e:
-				fields.append('UNKNOWN')
+                # parse mac address and look up the organization from the vendor octets
+                if mac_info:
+                        try:
+                                parsed_mac = netaddr.EUI(packet.addr2)
+                                fields.append('"' + parsed_mac.oui.registration().org + '"')
+                        except netaddr.core.NotRegisteredError, e:
+                                fields.append('"UNKNOWN"')
 
-		# include the SSID in the probe frame
-		if ssid:
-			fields.append(packet.info)
-			
-		if rssi:
-			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
-			fields.append(str(rssi_val))
+                # include the SSID in the probe frame
+                if ssid:
+                        fields.append('"' + packet.info + '"')
+                if rssi:
+                        rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
+                        fields.append('"' + str(rssi_val) + '"')
 
-		logger.info(delimiter.join(fields))
+                logger.info(delimiter.join(fields))
+
 
 	return packet_callback
 
@@ -73,6 +77,7 @@ def main():
 	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
 	parser.add_argument('-D', '--debug', action='store_true', help="enable debug output")
 	parser.add_argument('-l', '--log', action='store_true', help="enable scrolling live view of the logfile")
+	parser.add_argument('-e', '--exclude', default='exclude.conf', help="list of MAC addresses to exclude from output, one MAC per line")
 	args = parser.parse_args()
 
 	if not args.interface:
@@ -80,7 +85,7 @@ def main():
 		sys.exit(-1)
 	
 	DEBUG = args.debug
-
+	exclude = numpy.genfromtxt(args.exclude, delimiter="\t", dtype=None)
 	# setup our rotating logger
 	logger = logging.getLogger(NAME)
 	logger.setLevel(logging.INFO)
